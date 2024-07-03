252932787cbac18f531bea02265d2a553d53d79d
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-07-03 16:53:46 +0530

    threading: basic executor service

--- threading/src/main/java/org/abhinavgpt/Main.java
+++ threading/src/main/java/org/abhinavgpt/Main.java
@@ -4,16 +4,27 @@ import org.abhinavgpt.threads.EmptyThread;
 import org.abhinavgpt.threads.NumberPrinter;
 import org.abhinavgpt.threads.SequencePrinter;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Threading running the main method is:" + Thread.currentThread().getName());
 
-        for(int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 100; i++) {
             NumberPrinter numberPrinter = new NumberPrinter(i);
             numberPrinter.start();
         }
 
         EmptyThread emptyThread = new EmptyThread();
         emptyThread.start();
+
+//        Manages and re-uses threads!
+        ExecutorService es = Executors.newFixedThreadPool(10);
+        for (int i = 1; i <= 100; i++) {
+            es.execute(new NumberPrinter(i));
+        }
+
+        es.close();
     }
 }
