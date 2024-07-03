fbc35f50f1b6baaf4750015948b5fd92e733837a
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-07-03 15:21:03 +0530

    threading: init

---
+++ threading/src/main/java/org/abhinavgpt/Main.java
@@ -0,0 +1,7 @@
+package org.abhinavgpt;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Threading running the main method is:" + Thread.currentThread().getName());
+    }
+}
